d4c3cf5de791d02fb8d8f7ea141932cdb59b4d78
Samson Zhang <wwsamson@yahoo.com>
2023-04-11 14:26:51 -0700

    update code

--- basic/basic.ino
+++ basic/basic.ino
@@ -1,24 +1,57 @@
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+const int chipSelect = 4;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A3, OUTPUT);
+  // pinMode(A0, INPUT);
+  // pinMode(A1, OUTPUT);
   Serial.begin(9600);
+
+  // from https://docs.arduino.cc/learn/programming/sd-guide
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // read of 0.76V => 236 Arduino units
   float analogToVoltageDenom = 310.5;
-  int a1analog = analogRead(A1);
-  int a2analog = analogRead(A2);
-  float a1voltage = a1analog / analogToVoltageDenom;
-  float a2voltage = a2analog / analogToVoltageDenom;
-  Serial.println("A1 voltage: " + String(a1voltage));
-  Serial.println("A2 voltage: " + String(a2voltage));
-  // if (a2voltage < 2.65) {
-  //   analogWrite(3, 255);
-  // } else {
-  //   analogWrite(3, 0);
-  // }
-  analogWrite(A3, 256);
+  float temperatureVoltage = analogRead(A3) / analogToVoltageDenom; // 
+  float pressureVoltage = analogRead(A10) / analogToVoltageDenom;
+  float salinityMaxVoltage = analogRead(A0) / analogToVoltageDenom;
+  float salinityMinVoltage = analogRead(A1) / analogToVoltageDenom;
+  Serial.println("Temperature: " + String(temperatureVoltage));
+  Serial.println("Pressure: " + String(temperatureVoltage));
+  Serial.println("Salinity max: " + String(salinityMaxVoltage));
+  Serial.println("Salinity min: " + String(salinityMinVoltage));
+  if (pressureVoltage < 2.65) {
+    // digitalWrite(24, HIGH);
+    // digitalWrite(25, LOW);
+  } else {
+    // digitalWrite(24, LOW);
+    // digitalWrite(25, HIGH);
+  }
+  myFile = SD.open("test.txt", FILE_WRITE);
+  if (myFile) {
+    Serial.println("Writing to SD card...");
+    myFile.print(millis());
+    myFile.print(",");
+    myFile.print(pressureVoltage);
+    myFile.println();
+    myFile.close();
+    Serial.println("done writing to SD card");
+  } else {
+    Serial.println("Failed to open file");
+  }
   delay(250);
 }
